ef94b6045888c197da7d98b63ae8132e9ec82862
pedrofonseca1227 <henrriquemaluko@gmail.com>
2025-12-02 18:12:59 -0300

    Habilitar cobertura de código

--- GadoApi/obj/Debug/net10.0/GadoApi.AssemblyInfo.cs
+++ GadoApi/obj/Debug/net10.0/GadoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GadoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7996ad5321f6fb0d015edc34818e87ee58099d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2973a61856ff9af2d5454516e029f31499d8e070")]
 [assembly: System.Reflection.AssemblyProductAttribute("GadoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GadoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
