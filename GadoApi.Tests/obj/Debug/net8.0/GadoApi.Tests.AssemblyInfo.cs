d595e3544ad60d687fc0cbf9c579fe1cdc7cc7df
pedrofonseca1227 <henrriquemaluko@gmail.com>
2025-12-03 15:18:16 -0300

    adicionando collector

--- GadoApi.Tests/obj/Debug/net8.0/GadoApi.Tests.AssemblyInfo.cs
+++ GadoApi.Tests/obj/Debug/net8.0/GadoApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GadoApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ff74e1af4d8f7ea6e26dfa3e224d6658245a49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb73115773998e876f07620c7dc73fc2ba333425")]
 [assembly: System.Reflection.AssemblyProductAttribute("GadoApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GadoApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
